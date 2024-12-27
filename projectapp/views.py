157c43778c3734b1e432bbbb83a73d3c35ab4557
osungy69 <osungy69@gmail.com>
2024-12-27 21:59:12 +0900

    42

--- projectapp/views.py
+++ projectapp/views.py
@@ -10,6 +10,7 @@ from django.views.generic.list import MultipleObjectMixin
 from articleapp.models import Article
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
+from subscribeapp.models import Subscription
 
 
 @method_decorator(login_required, 'get')
@@ -31,8 +32,22 @@ class ProjectDetailView(DetailView, MultipleObjectMixin):
     paginate_by = 25
 
     def get_context_data(self, **kwargs):
+        project = self.object
+        user = self.request.user
+
+        # subscription 초기값 설정
+        subscription = None
+
+        if user.is_authenticated:
+            subscription = Subscription.objects.filter(user=user, project=project).first()  # 적절한 값을 설정
+
         object_list = Article.objects.filter(project=self.get_object())
-        return super(ProjectDetailView, self).get_context_data(object_list=object_list, **kwargs)
+
+        return super(ProjectDetailView, self).get_context_data(
+            object_list=object_list,
+            subscription=subscription,
+            **kwargs
+        )
 
 
 class ProjectListView(ListView):
